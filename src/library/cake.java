e5517758f7407ca2f4feeddad510db2793c5c040
harshniture <harshniture12@gmail.com>
2023-06-05 23:06:28 +0530

    test2

---
+++ src/library/cake.java
@@ -0,0 +1,20 @@
+// Making the Scanner available to the program
+import java.util.Scanner;
+
+public class cake {
+    public static void main(String[] main) {
+        // Creating the Scanner
+        Scanner reader = new Scanner(System.in);
+
+        // Declaring the variables and assigning user input to them
+        int first = Integer.valueOf(reader.nextLine());
+        int second = Integer.valueOf(reader.nextLine());
+
+        // Identifying the operation and declaring a variable for the result
+        int product = first * second;
+
+        // Printing the result of the operation
+        System.out.println("The product of " + first + " and " + second + " is " + product);
+
+    }
+}
